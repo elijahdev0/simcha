323d1e937418ce1a1c3b35a561eff93bf793e2e0
Some1private <ma443rcus@gmail.com>
2025-03-11 23:41:48 +0200

    Dfdf

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig(({ mode }) => ({
     host: "::",
     port: 8080,
   },
+  preview: {
+    host: "::",
+    port: 4173,
+    allowedHosts: ["baldeagletactical.com"],
+  },
   plugins: [
     react(),
     mode === 'development' &&
